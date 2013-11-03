9b584294559d412c7e525ccdff1ab72649ea69b9
Hongfei <monkeychf@gmail.com>
2013-11-03 11:54:25 -0600

    post status

--- test.py
+++ test.py
@@ -1,27 +1,6 @@
 #!/usr/bin/env python
 
-# import twitter
-
-# api = twitter.Api()
-# 
-# api = twitter.Api(consumer_key='O5Z1KINSBaDEQgTBB3FA',
-# 	consumer_secret='kf42pzVoDrmP4hoe9LNQW5t765J2zEspqdHQhotw',
-# 	access_token_key='2172397754-TrCNJSgVarJIMnYb6uLXFzfI76zI7m8cfy5zIvL',
-# 	access_token_secret='FNiDwwbgs0WiElgaJ2hVECnsyavUKIQ3IP7JNr4BkiOhx')
-# 	                     
-# print api.VerifyCredentials()
-
-
-# token = 'FNiDwwbgs0WiElgaJ2hVECnsyavUKIQ3IP7JNr4BkiOhx'
-# token_key = '2172397754-TrCNJSgVarJIMnYb6uLXFzfI76zI7m8cfy5zIvL'
-# con_secret = 'kf42pzVoDrmP4hoe9LNQW5t765J2zEspqdHQhotw'
-# con_secret_key = 'O5Z1KINSBaDEQgTBB3FA'
-# 
-# # from twitter import Twitter
-# twitter = Twitter(auth=OAuth(token, token_key, con_secret, con_secret_key))
-# twitter.statuses.public_timeline()
-
-
+import json
 from twython import Twython
 
 APP_KEY = 'O5Z1KINSBaDEQgTBB3FA'
@@ -30,6 +9,24 @@ OAUTH_TOKEN = '2172397754-TrCNJSgVarJIMnYb6uLXFzfI76zI7m8cfy5zIvL'
 OAUTH_TOKEN_SECRET = 'FNiDwwbgs0WiElgaJ2hVECnsyavUKIQ3IP7JNr4BkiOhx'
 
 twitter = Twython(APP_KEY, APP_SECRET,OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+# 
+# print twitter.search(q='twitter')
+# print twitter.search(q='twitter', result_type='popular')
+# print twitter.search(q='python', result_type='popular')
+# print twitter.verify_credentials()
+# twitter.update_status(status='See how easy using Twython is!')
 print twitter.get_home_timeline()
-print twitter.search(q='twitter')
-print twitter.search(q='twitter', result_type='popular')
+content = twitter.search(q='geo', result_type='popular')
+print content
+# parsed = json.loads(content)
+# json.dumps(parsed, indent=4, sort_keys=True)
+
+# class MyStreamer(TwythonStreamer):
+# 	def on_success(self, data):
+# 		if 'text' in data:
+# 			print data['text'].encode('utf-8')
+# 	def on_error(self, status_code, data):
+# 		print status_code
+# 		
+# stream = MyStreamer(APP_KEY, APP_SECRET,OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+# stream.statuses.filter(track='twitter')
