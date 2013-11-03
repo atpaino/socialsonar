64e78bfd068f87e834ff1fbe59eb749e5a237cab
Alex Paino <atpaino@gmail.com>
2013-11-03 19:05:08 +0000

    Skeleton of ping view completed

--- socialsonar/views.py
+++ socialsonar/views.py
@@ -1,5 +1,7 @@
 # Create your views here.
 
+import json
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -7,6 +9,19 @@ def home(request):
       return render(request, 'socialsonar/index.html', {})
 
 def ping(request, latitude, longitude):
+      # Convert latitude and longitude to floating point type
       latitude = float(latitude)
       longitude = float(longitude)
-      return HttpResponse('Huhro ping from {}, {}'.format(latitude, longitude))
+
+      # Query twitter API
+      tweets = []
+      # TODO
+
+      # Create list of tweets, sorted by theta
+      data = []
+      for tweet in tweets:
+          data.append('hey')
+      # TODO
+
+      # Build a JSON response out of it
+      return HttpResponse(json.dumps(data), content_type="application/json")
